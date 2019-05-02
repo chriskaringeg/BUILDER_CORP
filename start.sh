22b23aecf06e553fa08f6d18fd1352c1aca147ea
IanSani <iansani259@gmail.com>
2019-05-02 17:47:46 +0300

    Added video background

---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME='ckaringe@gmail.com'
+export MAIL_PASSWORD='blackpool005'
+
+python3.6 manage.py server
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/chris/Documents/Build-corp/virtual"
+set -gx VIRTUAL_ENV "/home/ain/core/BUILDER_CORP/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
