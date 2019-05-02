437dcb81c94a1e4c8e9e6c93f6d34f068eb3d91c
IanSani <iansani259@gmail.com>
2019-05-02 23:16:59 +0300

    Added constructors profiles

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,30 +1,49 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template ,flash, redirect,url_for,request
+from flask_login import login_user,logout_user,login_required
 from . import auth
-from ..models import User
-from flask_login import login_required
-from flask_login import login_required, current_user
+from .. import main
+from ..models import *
+from .forms import *
+from .. import db
+from app.main import views
 
 
-@auth.route('/')
-def index():
-    return render_template('index.html')
+@auth.route('/register',methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('auth.login'))
+		title="New Account"
+
+	return render_template('auth/register.html',registration_form = form)
+
 
 @auth.route('/professionals')
-
 def professionals():
     return render_template('professionals.html')
 
-@auth.route('/architect')
-@login_required
-def architect():
-    return render_template('architects.html')
+@auth.route('/login', methods=['GET','POST'])
+def login():
+	login_form= LoginForm()
+	if login_form.validate_on_submit():
+		user=User.query.filter_by(email = login_form.email.data).first()
+		if user is not None and user.verify_password(login_form.password.data):
+			login_user(user,login_form.remember.data)
+			return redirect(request.args.get('next') or url_for('main.index'))
+		flash('Invalid Password')
+		return redirect(url_for('auth.professionals'))
 
-@auth.route('/engineers')
-@login_required
-def engineers():
-    return render_template('engineers.html')
 
-@auth.route('/constructors')
+	title = "builders login"
+	return render_template('auth/login.html', login_form = login_form,title = title)
+
+
+
+@auth.route('/logout')
 @login_required
-def constructors():
-    return render_template('constructors.html')
+def logout():
+	logout_user()
+	return redirect(url_for("main.index"))
